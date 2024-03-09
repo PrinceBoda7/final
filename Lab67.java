fb4d80d7297cf97f1f8ca3a7f68dca65e466e4a8
PrinceBoda7 <162782524+PrinceBoda7@users.noreply.github.com>
2024-03-09 13:10:13 +0530

    Add files via upload

---
+++ Lab67.java
@@ -0,0 +1,17 @@
+class A {
+	final void C() {
+		System.out.println("Final");
+	}
+}
+
+class B extends A {
+	void C() {
+		System.out.println("Not Final");
+	}
+}
+
+public class Lab67 {
+	public static void main(String[] args) {
+
+	}
+}
